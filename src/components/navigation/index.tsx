2cc91669b8441042882cac6d9682c2d5632d8eef
infosetGang <kangyoonkyung@infoset.co.kr>
2024-10-21 15:15:28 +0900

    [feat] 메인 네비게이션 리팩토링

--- src/components/navigation/index.tsx
+++ src/components/navigation/index.tsx
@@ -1,30 +1,22 @@
 import React from 'react';
-import { Link, useLocation } from '@tanstack/react-router';
 import styles from './index.module.css';
+import { Link, useLocation } from '@tanstack/react-router';
+import { NAVIGATION_LIST } from '../../constants/navigation.constants';
 
 const Navigation: React.FC = () => {
-  const location = useLocation();
-  const currentPath = location.pathname;
+  const { pathname } = useLocation();
 
   return (
     <div className={styles.navigation}>
       <h1 className={styles.header}>인포셋도서관</h1>
       <ul className={styles.menu}>
-        <li>
-          <Link className={currentPath === '/' ? styles.active : ''} to={'/'}>
-            미디어월
-          </Link>
-        </li>
-        <li>
-          <Link className={currentPath === '/kiosk' ? styles.active : ''} to={'/kiosk'}>
-            이용안내키오스크
-          </Link>
-        </li>
-        <li>
-          <Link className={currentPath === '/smart' ? styles.active : ''} to={'/smart'}>
-            스마트추천도서
-          </Link>
-        </li>
+        {NAVIGATION_LIST.map(({ path, label }) => (
+          <li key={path}>
+            <Link className={pathname === path ? styles.active : ''} to={path}>
+              {label}
+            </Link>
+          </li>
+        ))}
       </ul>
     </div>
   );
---
+++ src/constants/navigation.constants.ts
@@ -0,0 +1,5 @@
+export const NAVIGATION_LIST = [
+  { path: '/', label: '미디어월' },
+  { path: '/kiosk', label: '이용안내키오스크' },
+  { path: '/smart', label: '스마트추천도서' },
+];
