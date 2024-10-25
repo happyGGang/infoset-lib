d8b8291cf442322de8a4578177f318d42e4d0df4
infosetGang <kangyoonkyung@infoset.co.kr>
2024-10-25 15:08:25 +0900

    [feat] 이용안내 추가

--- src/components/kiosk/type_a/information.tsx
+++ src/components/kiosk/type_a/information.tsx
@@ -1,4 +1,8 @@
 import React from 'react';
+import styles from './information.module.css';
+import section_0 from '../../../assets/img/kiosk/type_a/section_0.svg';
+import section_1 from '../../../assets/img/kiosk/type_a/section_1.svg';
+import section_2 from '../../../assets/img/kiosk/type_a/section_2.svg';
 
 interface Props {
   isFullScreen: boolean;
@@ -8,15 +12,29 @@ interface Props {
 }
 
 const HorizontalMode: React.FC = () => {
-  return <div>가로 모드입니다!</div>;
+  return (
+    <div className={styles.container_x}>
+      <div className={styles.content}>
+        <div className={styles.scroll}>
+          <div className={styles.left}>
+            <img src={section_0} className={styles.time} alt="" />
+            <img src={section_1} className={styles.rest} alt="" />
+          </div>
+          <div className={styles.right}>
+            <img src={section_2} className={styles.return} alt="" />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 const VerticalMode: React.FC = () => {
-  return <div>세로 모드입니다!</div>;
+  return <div className={styles.container_y}></div>;
 };
 
-const NoticeA: React.FC<Props> = ({ horizontalMode }) => {
+const InformationA: React.FC<Props> = ({ horizontalMode }) => {
   return <>{horizontalMode ? <HorizontalMode /> : <VerticalMode />}</>;
 };
 
-export default NoticeA;
+export default InformationA;
