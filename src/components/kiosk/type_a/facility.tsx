20c94092a77560025d6489b1ca7eabae0befaab6
infosetGang <kangyoonkyung@infoset.co.kr>
2024-10-28 16:43:35 +0900

    [feat] 시설 안내 팝업 작업 완료

--- src/components/kiosk/type_a/facility.tsx
+++ src/components/kiosk/type_a/facility.tsx
@@ -6,16 +6,19 @@ import map_1 from '../../../assets/img/kiosk/type_a/map_1F.png';
 import map_2 from '../../../assets/img/kiosk/type_a/map_2F.svg';
 import map_3 from '../../../assets/img/kiosk/type_a/map_3F.svg';
 import { Pagination } from 'swiper/modules';
-import { FACILITY_A, FACILITY_B, NOTICE } from '../../../constants/kiosk.constants';
+import { FACILITY_A, FACILITY_B } from '../../../constants/kiosk.constants';
 import { Swiper, SwiperSlide } from 'swiper/react';
 import 'swiper/css';
 import 'swiper/css/pagination';
 import Tilt from '../../tilt';
 import Full from '../../full_screen';
+import popup_content_y from '../../../assets/img/kiosk/type_a/facility_popup_content_y.svg';
+import close from '../../../assets/img/kiosk/type_a/close.svg';
 
 const HorizontalMode: React.FC = () => {
   const [selectedFloor, setSelectedFloor] = useState(1);
   const [animate, setAnimate] = useState(false);
+  const [isOpen, setIsOpen] = useState(false);
 
   const mapImages: { [key: number]: string } = { 1: map_1, 2: map_2, 3: map_3 };
 
@@ -37,61 +40,82 @@ const HorizontalMode: React.FC = () => {
   }, [selectedFloor]);
 
   return (
-    <div className={styles.container_x}>
-      <div className={styles.left}>
-        <div className={styles.map_container_x}>
-          <img className={styles.map_x} src={mapImages[selectedFloor]} alt={`${selectedFloor}F`} />
-          <div className={`${styles.selected_floor_x} ${animate ? styles.animate : ''}`}>
-            {selectedFloor}F
+    <>
+      {isOpen ? (
+        <div className={styles.popup_x}>
+          <div onClick={() => setIsOpen(false)} className={styles.close_x}>
+            <img src={close} alt="" />
+            닫기
           </div>
         </div>
-
-        <div className={styles.floor_list_x}>
-          {Array.from({ length: 3 }, (_, index) => {
-            const floorNumber = index + 1;
-            const isActive = selectedFloor === floorNumber;
-            const lineImage = isActive ? yellowLine : grayLine;
-            const activeClass = isActive ? styles.active : '';
-
-            return (
-              <div
-                key={floorNumber}
-                onClick={() => setSelectedFloor(floorNumber)}
-                className={`${styles.floor_btn_x} ${activeClass}`}
-              >
-                <div>{floorNumber}F</div>
-                <img src={lineImage} alt="" />
+      ) : (
+        <div className={styles.container_x}>
+          <div className={styles.left}>
+            <div className={styles.map_container_x}>
+              <img
+                className={styles.map_x}
+                src={mapImages[selectedFloor]}
+                alt={`${selectedFloor}F`}
+              />
+              <div className={`${styles.selected_floor_x} ${animate ? styles.animate : ''}`}>
+                {selectedFloor}F
               </div>
-            );
-          })}
-        </div>
-      </div>
+            </div>
 
-      <Swiper
-        loop
-        direction={'vertical'}
-        slidesPerView={1}
-        slidesPerGroup={1}
-        pagination={pagination}
-        modules={[Pagination]}
-        className={styles.swiper_x}
-      >
-        {FACILITY_B.map((item, index) => (
-          <SwiperSlide
-            key={item.id}
-            style={{ width: 'width: 21.67969rem;', height: '30.46938rem' }}
+            <div className={styles.floor_list_x}>
+              {Array.from({ length: 3 }, (_, index) => {
+                const floorNumber = index + 1;
+                const isActive = selectedFloor === floorNumber;
+                const lineImage = isActive ? yellowLine : grayLine;
+                const activeClass = isActive ? styles.active : '';
+
+                return (
+                  <div
+                    key={floorNumber}
+                    onClick={() => setSelectedFloor(floorNumber)}
+                    className={`${styles.floor_btn_x} ${activeClass}`}
+                  >
+                    <div>{floorNumber}F</div>
+                    <img src={lineImage} alt="" />
+                  </div>
+                );
+              })}
+            </div>
+          </div>
+
+          <Swiper
+            loop
+            direction={'vertical'}
+            slidesPerView={1}
+            slidesPerGroup={1}
+            pagination={pagination}
+            modules={[Pagination]}
+            className={styles.swiper_x}
           >
-            <img src={item.img} alt="" className={styles.swiper_slide_x} />
-          </SwiperSlide>
-        ))}
-      </Swiper>
-    </div>
+            {FACILITY_B.map((item, index) => (
+              <SwiperSlide
+                key={item.id}
+                style={{ width: 'width: 21.67969rem;', height: '30.46938rem' }}
+              >
+                <img
+                  src={item.img}
+                  alt=""
+                  className={styles.swiper_slide_x}
+                  onClick={() => setIsOpen(true)}
+                />
+              </SwiperSlide>
+            ))}
+          </Swiper>
+        </div>
+      )}
+    </>
   );
 };
 
 const VerticalMode: React.FC = () => {
-  const [selectedFloor, setSelectedFloor] = useState(1);
+  const [selectedFloor, setSelectedFloor] = useState(2);
   const [animate, setAnimate] = useState(false);
+  const [isOpen, setIsOpen] = useState(false);
 
   const mapImages: { [key: number]: string } = { 1: map_1, 2: map_2, 3: map_3 };
 
@@ -113,52 +137,73 @@ const VerticalMode: React.FC = () => {
   }, [selectedFloor]);
 
   return (
-    <div className={styles.container_y}>
-      <div className={styles.map_container_y}>
-        <img className={styles.map_y} src={mapImages[selectedFloor]} alt={`${selectedFloor}F`} />
-        <div className={`${styles.selected_floor_y} ${animate ? styles.animate : ''}`}>
-          {selectedFloor}F
+    <>
+      {isOpen ? (
+        <div className={styles.popup_y}>
+          <img src={popup_content_y} alt="" className={styles.popup_content_y} />
+          <div onClick={() => setIsOpen(false)} className={styles.close_y}>
+            <img src={close} alt="" />
+            닫기
+          </div>
         </div>
-      </div>
-
-      <Swiper
-        loop
-        slidesPerView={1}
-        slidesPerGroup={1}
-        pagination={pagination}
-        modules={[Pagination]}
-        className={styles.swiper_y}
-      >
-        {FACILITY_A.map((item, index) => (
-          <SwiperSlide
-            key={item.id}
-            style={{ width: '19.76838rem !important', height: '12.08177rem' }}
-          >
-            <img src={item.img} alt="" className={styles.swiper_slide_y} />
-          </SwiperSlide>
-        ))}
-      </Swiper>
-
-      <div className={styles.floor_list_y}>
-        {Array.from({ length: 3 }, (_, index) => {
-          const floorNumber = index + 1;
-          const isActive = selectedFloor === floorNumber;
-          const lineImage = isActive ? yellowLine : grayLine;
-          const activeClass = isActive ? styles.active : '';
-
-          return (
-            <div
-              key={floorNumber}
-              onClick={() => setSelectedFloor(floorNumber)}
-              className={`${styles.floor_btn_y} ${activeClass}`}
-            >
-              <div>{floorNumber}F</div>
-              <img src={lineImage} alt="" />
+      ) : (
+        <div className={styles.container_y}>
+          <div className={styles.map_container_y}>
+            <img
+              className={styles.map_y}
+              src={mapImages[selectedFloor]}
+              alt={`${selectedFloor}F`}
+            />
+            <div className={`${styles.selected_floor_y} ${animate ? styles.animate : ''}`}>
+              {selectedFloor}F
             </div>
-          );
-        })}
-      </div>
-    </div>
+          </div>
+
+          <Swiper
+            loop
+            slidesPerView={1}
+            slidesPerGroup={1}
+            pagination={pagination}
+            modules={[Pagination]}
+            className={styles.swiper_y}
+          >
+            {FACILITY_A.map((item, index) => (
+              <SwiperSlide
+                key={item.id}
+                style={{ width: '19.76838rem !important', height: '12.08177rem' }}
+              >
+                <img
+                  src={item.img}
+                  alt=""
+                  className={styles.swiper_slide_y}
+                  onClick={() => setIsOpen(true)}
+                />
+              </SwiperSlide>
+            ))}
+          </Swiper>
+
+          <div className={styles.floor_list_y}>
+            {Array.from({ length: 3 }, (_, index) => {
+              const floorNumber = index + 1;
+              const isActive = selectedFloor === floorNumber;
+              const lineImage = isActive ? yellowLine : grayLine;
+              const activeClass = isActive ? styles.active : '';
+
+              return (
+                <div
+                  key={floorNumber}
+                  onClick={() => setSelectedFloor(floorNumber)}
+                  className={`${styles.floor_btn_y} ${activeClass}`}
+                >
+                  <div>{floorNumber}F</div>
+                  <img src={lineImage} alt="" />
+                </div>
+              );
+            })}
+          </div>
+        </div>
+      )}
+    </>
   );
 };
 
