a1a0bb5e0dda28c8584982d00910cf34142094a9
infosetGang <kangyoonkyung@infoset.co.kr>
2024-10-29 10:23:41 +0900

    [feat] 이용안내 작업 완료

--- src/components/kiosk/type_b/information.tsx
+++ src/components/kiosk/type_b/information.tsx
@@ -1,27 +1,10 @@
 import React, { useState } from 'react';
 import styles from './information.module.css';
-import section_0 from '../../../assets/img/kiosk/type_a/section_0.svg';
-import section_1 from '../../../assets/img/kiosk/type_a/section_1.svg';
-import section_2 from '../../../assets/img/kiosk/type_a/section_2.svg';
 import Tilt from '../../tilt';
 import Full from '../../full_screen';
 
 const HorizontalMode: React.FC = () => {
-  return (
-    <div className={styles.container_x}>
-      <div className={styles.content}>
-        <div className={styles.scroll}>
-          <div className={styles.left}>
-            <img src={section_0} className={styles.time} alt="" />
-            <img src={section_1} className={styles.rest} alt="" />
-          </div>
-          <div className={styles.right}>
-            <img src={section_2} className={styles.return} alt="" />
-          </div>
-        </div>
-      </div>
-    </div>
-  );
+  return <div className={styles.container_x}></div>;
 };
 
 const VerticalMode: React.FC = () => {
