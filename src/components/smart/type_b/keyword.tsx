5aeca9999a3b847a260796f0536a3bab672184d8
infosetGang <kangyoonkyung@infoset.co.kr>
2024-10-30 18:04:33 +0900

    [feat] 탭바 커스텀 완료

--- src/components/smart/type_b/keyword.tsx
+++ src/components/smart/type_b/keyword.tsx
@@ -2,13 +2,116 @@ import React, { useState } from 'react';
 import styles from './keyword.module.css';
 import Tilt from '../../tilt';
 import Full from '../../full_screen';
+import { Tabs, Tab, Box, Typography } from '@mui/material';
 
 const HorizontalMode: React.FC = () => {
   return <div className={styles.container_x}></div>;
 };
 
+// TabPanel 컴포넌트 정의
+function TabPanel(props: { children?: React.ReactNode; index: number; value: number }) {
+  const { children, value, index, ...other } = props;
+
+  return (
+    <div
+      role="tabpanel"
+      hidden={value !== index}
+      id={`simple-tabpanel-${index}`}
+      aria-labelledby={`simple-tab-${index}`}
+      {...other}
+    >
+      {value === index && (
+        <Box sx={{ p: 3 }}>
+          <Typography>{children}</Typography>
+        </Box>
+      )}
+    </div>
+  );
+}
+
 const VerticalMode: React.FC = () => {
-  return <div className={styles.container_y}></div>;
+  const [value, setValue] = useState(0);
+
+  const handleChange = (event: React.SyntheticEvent, newValue: number) => {
+    setValue(newValue);
+  };
+
+  return (
+    <div className={styles.container_y}>
+      <Tabs
+        value={value}
+        onChange={handleChange}
+        className={styles.tabs_y}
+        sx={{
+          width: '19.75725rem',
+          borderBottom: '1px solid #191F28',
+          minHeight: '1.49rem',
+          '& .MuiTabs-indicator': {
+            backgroundColor: '#7B3AD8',
+            maxHeight: '0.05rem',
+          },
+        }}
+      >
+        <Tab
+          label="키워드추천"
+          className={styles.tab_y}
+          sx={{
+            minHeight: '1.49rem',
+            height: '1.49rem',
+            minWidth: '6.58575rem',
+            padding: '1.1rem 0 1.1rem 0',
+            fontSize: '0.54881rem',
+            color: '#6B7684',
+            fontWeight: '700',
+            '&.Mui-selected': {
+              color: '#191F28',
+            },
+          }}
+        />
+        <Tab
+          label="사서추천"
+          className={styles.tab_y}
+          sx={{
+            minHeight: '1.49rem',
+            height: '1.49rem',
+            minWidth: '6.58575rem',
+            padding: '1.1rem 0 1.1rem 0',
+            fontSize: '0.54881rem',
+            color: '#6B7684',
+            fontWeight: '700',
+            '&.Mui-selected': {
+              color: '#191F28',
+            },
+          }}
+        />
+        <Tab
+          label="빅데이터추천"
+          className={styles.tab_y}
+          sx={{
+            minHeight: '1.49rem',
+            height: '1.49rem',
+            minWidth: '6.58575rem',
+            padding: '1.1rem 0 1.1rem 0',
+            fontSize: '0.54881rem',
+            color: '#6B7684',
+            fontWeight: '700',
+            '&.Mui-selected': {
+              color: '#191F28',
+            },
+          }}
+        />
+      </Tabs>
+      <TabPanel value={value} index={0}>
+        첫 번째 아이템의 내용입니다.
+      </TabPanel>
+      <TabPanel value={value} index={1}>
+        두 번째 아이템의 내용입니다.
+      </TabPanel>
+      <TabPanel value={value} index={2}>
+        세 번째 아이템의 내용입니다.
+      </TabPanel>
+    </div>
+  );
 };
 
 const KeywordB: React.FC = () => {
