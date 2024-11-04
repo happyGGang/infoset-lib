c5f1fb160e89a3b1b2a7ff1631212a5b0df8f341
infosetGang <kangyoonkyung@infoset.co.kr>
2024-11-04 11:51:32 +0900

    [feat] 책속 한줄 더미 데이터 추가

--- src/components/media/type_b/line_of_book.tsx
+++ src/components/media/type_b/line_of_book.tsx
@@ -1,10 +1,36 @@
 import styles from './line_of_book.module.css';
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import Full from '../../full_screen';
 import { useFullPageStore } from '../../../store/full_page.store';
 
 const LineOfBookB: React.FC = () => {
   const { toggleFullPage } = useFullPageStore();
+
+  const data = [
+    {
+      title: '그림 안의 시간은 한순간에 얼어붙었다기보다\n흘러들어 고인 느낌이다.',
+      des: "'나는 메트로폴리탄 미술관의 경비원입니다' 중에서",
+    },
+    {
+      title: '우리는 인생의 날들을 늘릴 수는 없지만, 그 날들에\n생기를 불어넣을 수는 있다.',
+      des: "'당신의 완벽한 1년’ 중에서",
+    },
+  ];
+
+  const [currentIndex, setCurrentIndex] = useState(0);
+  const [animate, setAnimate] = useState(true);
+
+  useEffect(() => {
+    const intervalId = setInterval(() => {
+      setAnimate(false);
+      setTimeout(() => {
+        setCurrentIndex((prevIndex) => (prevIndex + 1) % data.length);
+        setAnimate(true);
+      }, 50);
+    }, 10000);
+
+    return () => clearInterval(intervalId);
+  }, []);
 
   return (
     <div className={styles.wrapper}>
@@ -14,11 +40,12 @@ const LineOfBookB: React.FC = () => {
           <div className={styles.header_title_en}>A LINE OF BOOK</div>
         </div>
         <div className={styles.title}>책 속의 한줄 발견</div>
-        <div className={styles.line_of_book}>
-          그림 안의 시간은 한순간에 얼어붙었다기보다 <br />
-          흘러들어 고인 느낌이다.
+        <div className={`${styles.line_of_book} ${animate ? styles.animate : ''}`}>
+          {data[currentIndex].title}
         </div>
-        <div className={styles.caption}>'나는 메트로폴리탄 미술관의 경비원입니다' 중에서</div>
+        <div className={`${styles.caption} ${animate ? styles.animate : ''}`}>
+          {data[currentIndex].des}
+        </div>
       </div>
       <Full disabled={false} onClick={toggleFullPage} />
     </div>
